af74814a0e1c95d3871ba8f276b5859968b23d7c
PerLinus <linus.nilsson83@gmail.com>
2017-05-26 12:01:02 +0200

    Initial commit

---
+++ src/main/java/com/projekt/rezt/ReztApplication.java
@@ -0,0 +1,12 @@
+package com.projekt.rezt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReztApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReztApplication.class, args);
+	}
+}
